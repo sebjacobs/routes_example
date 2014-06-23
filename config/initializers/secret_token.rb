49a8f1020609521e8b19f166d24a5e792ace2004
Seb Jacobs <me@sebjacobs.com>
2014-06-23 15:00:46 +0100

    Add all the things!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoutesExample::Application.config.secret_key_base = '80225b11fae2d82c0746df244c24a76f05405584a151e722c780b868576ec623a357d36c47e00a89d84b8e9aa240ab208d54d0790b5b51bd4e9b2791c6d5369e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RoutesExample::Application.config.session_store :cookie_store, key: '_routes_example_session'
